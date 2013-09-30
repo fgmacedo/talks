d4821e1b66250efbfd55711b44356327624b2d9a
Fernando Macedo <fgmacedo@gmail.com>
2013-09-30 00:01:08 -0300

    initial

---
+++ pybr9_raspador/converter.sh
@@ -0,0 +1,1 @@
+ipython nbconvert --to slides --RevealHelpTransformer.url_prefix=reveal.js  --config slides_config.py raspador.ipynb
---
+++ pybr9_raspador/slides_config.py
@@ -0,0 +1,2 @@
+c = get_config()
+c.Exporter.template_file = 'slides_reveal'
