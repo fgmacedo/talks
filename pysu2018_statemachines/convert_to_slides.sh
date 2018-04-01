347de698358b43f0c47866ea1b47a2be2daa2c6a
Fernando Macedo <fgmacedo@gmail.com>
2018-04-01 11:09:53 -0300

    fixing refs to reveal.js

--- pysu2018_statemachines/convert_to_slides.sh
+++ pysu2018_statemachines/convert_to_slides.sh
@@ -1,1 +1,3 @@
-jupyter nbconvert state_machines.ipynb --to slides  --reveal-prefix reveal.js --post serve --SlidesExporter.reveal_theme=simple
+export BROWSER=google-chrome
+
+jupyter nbconvert state_machines.ipynb --to slides  --reveal-prefix reveal.js.350 --post serve --SlidesExporter.reveal_theme=simple
